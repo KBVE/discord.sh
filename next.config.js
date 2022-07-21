c2c99c8bc5dd65a1bdbf4e085ba4b4c71f09036d
h0lybyte <admin@kbve.com>
2022-07-21 00:19:05 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "/discord.sh/" : "",
-};
