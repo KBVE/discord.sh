0102c16286795b59085c650f45931e4d9be30a96
h0lybyte <admin@kbve.com>
2022-07-21 01:55:02 -0400

    Create kbve.js

---
+++ www/pages/kbve.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Kbve() {
+  return (
+    <div>
+      <div>Kbve</div>
+      <div>
+        Back to <Link href="/">Home</Link>
+      </div>
+    </div>
+  )
+}
