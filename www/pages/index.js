609531cbc80d2b34dbabcd7500797c57388c4e37
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-21 00:23:15 -0500

    Testing new packages

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,9 +1,17 @@
 import Link from 'next/link'
+// @mui/material
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <Box p={4}>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h5">
+          Hello World. <Link href="/about">About</Link>
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
