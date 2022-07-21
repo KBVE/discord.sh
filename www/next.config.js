4ee8e17eaff0d58b0ca28b0859a88c99c34d2c99
h0lybyte <admin@kbve.com>
2022-07-21 00:29:41 -0400

    www

--- www/next.config.js
+++ www/next.config.js
@@ -1,9 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   */
-  assetPrefix: isProd ? "/discord.sh/" : "",
-};
+  //  basePath: '/discord.sh',
+  assetPrefix: isProd ? '/discord.sh/' : ''
+}
