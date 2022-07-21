0bbf5ca2f2dba8a18af03a0f89fa01ac1b394db7
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-21 00:31:15 -0500

    Testing Icons

--- www/pages/index.js
+++ www/pages/index.js
@@ -3,11 +3,14 @@ import Link from 'next/link'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+// @mui/icons-material
+import ComputerIcon from '@mui/icons-material/Computer'
 
 export default function Home() {
   return (
     <Box p={4}>
-      <Stack direction="column" spacing={2}>
+      <Stack direction="row" spacing={2} alignItems="center">
+        <ComputerIcon />
         <Typography variant="h5">
           Hello World. <Link href="/about">About</Link>
         </Typography>
