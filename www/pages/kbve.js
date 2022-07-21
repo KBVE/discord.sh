336f170bfd488108ba762c0d1fc17620e60d21cf
h0lybyte <admin@kbve.com>
2022-07-21 11:23:47 -0400

    Update kbve.js
    Adding back Ziggys SWR commit

--- www/pages/kbve.js
+++ www/pages/kbve.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import _ from 'lodash'
+import useSWR from 'swr'
 // @mui/material
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
@@ -9,18 +10,30 @@ import CircularProgress from '@mui/material/CircularProgress'
 // @mui/icons-material
 import ComputerIcon from '@mui/icons-material/Computer'
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Kbve() {
 
+  const { data, error } = useSWR(`https://api.publicapis.org/entries`, fetcher)
+
+  // From https://github.com/KBVE/discord.sh/commit/3d0b5f8cf2acc21aec964e5af5f3416875a25353
   return (
     <Box p={4}>
       <Stack direction="column" spacing={4}>
         <Stack direction="row" spacing={2} alignItems="center">
           <ComputerIcon />
           <Typography variant="h5">
-            Hello World. <Link href="/about">About</Link> <Link href="/">Home</Link>
+            KBVE. <Link href="/about">About</Link> <Link href="/">Home</Link>
           </Typography>
         </Stack>
-       
+        {data && !error
+          ? <Typography variant="pre">{JSON.stringify({ data })}</Typography>
+          : !data && !error
+            ? <CircularProgress />
+            : <Typography variant="pre" sx={{ color: 'red' }}>
+              {JSON.stringify({ error })}
+            </Typography>
+        }
       </Stack>
     </Box>
   )
