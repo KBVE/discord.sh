df2e27cff25ecc40d65d43c3b11901873c3f86fa
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-21 01:25:57 -0500

    Timeout on index

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import _ from 'lodash'
-import useSWR from 'swr'
+//import useSWR from 'swr'
 // @mui/material
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
@@ -13,7 +13,14 @@ import ComputerIcon from '@mui/icons-material/Computer'
 const fetcher = (url) => fetch(url).then((res) => res.json()); 0
 
 export default function Home() {
-  const { data, error } = useSWR(`https://api.publicapis.org/entries`, fetcher)
+
+  const [timedOut, setTimedOut] = React.useState(false)
+  React.useEffect(() => {
+    let timer1 = setTimeout(() => setTimedOut(true), 5 * 1000)
+    return () => {
+      clearTimeout(timer1)
+    }
+  }, [])
 
   return (
     <Box p={4}>
@@ -24,13 +31,9 @@ export default function Home() {
             Hello World. <Link href="/about">About</Link> <Link href="/kbve">KBVE</Link>
           </Typography>
         </Stack>
-        {data && !error
-          ? <Typography variant="pre">{JSON.stringify({ data })}</Typography>
-          : !data && !error
-            ? <CircularProgress />
-            : <Typography variant="pre" sx={{ color: 'red' }}>
-              {JSON.stringify({ error })}
-            </Typography>
+        {timedOut
+          ? <Typography variant="pre">{JSON.stringify({ timedOut })}</Typography>
+          : <CircularProgress />
         }
       </Stack>
     </Box>
