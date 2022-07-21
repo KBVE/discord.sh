e0fadbcbfa2b9767bc508da3feeda1f3180f5794
h0lybyte <admin@kbve.com>
2022-07-21 10:58:08 -0400

    assetPrefix?

--- www/pages/kbve.js
+++ www/pages/kbve.js
@@ -1,12 +1,30 @@
+import React from 'react'
 import Link from 'next/link'
+import _ from 'lodash'
+// @mui/material
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import CircularProgress from '@mui/material/CircularProgress'
+// @mui/icons-material
+import ComputerIcon from '@mui/icons-material/Computer'
 
 export default function Kbve() {
+
   return (
-    <div>
-      <div>Kbve</div>
-      <div>
-        Back to <Link href="/">Home</Link>
-      </div>
-    </div>
+    <Box p={4}>
+      <Stack direction="column" spacing={4}>
+        <Stack direction="row" spacing={2} alignItems="center">
+          <ComputerIcon />
+          <Typography variant="h5">
+            Hello World. <Link href="/about">About</Link> <Link href="/">Home</Link>
+          </Typography>
+        </Stack>
+        {timedOut
+          ? <Typography variant="pre">{JSON.stringify({ timedOut })}</Typography>
+          : <CircularProgress />
+        }
+      </Stack>
+    </Box>
   )
 }
