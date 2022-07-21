3d0b5f8cf2acc21aec964e5af5f3416875a25353
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-21 01:07:15 -0500

    Testing data fetching

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,19 +1,37 @@
+import React from 'react'
 import Link from 'next/link'
+import _ from 'lodash'
+import useSWR from 'swr'
 // @mui/material
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import CircularProgress from '@mui/material/CircularProgress'
 // @mui/icons-material
 import ComputerIcon from '@mui/icons-material/Computer'
 
+const fetcher = (url) => fetch(url).then((res) => res.json()); 0
+
 export default function Home() {
+  const { data, error } = useSWR(`https://api.publicapis.org/entries`, fetcher)
+
   return (
     <Box p={4}>
-      <Stack direction="row" spacing={2} alignItems="center">
-        <ComputerIcon />
-        <Typography variant="h5">
-          Hello World. <Link href="/about">About</Link>
-        </Typography>
+      <Stack direction="column" spacing={4}>
+        <Stack direction="row" spacing={2} alignItems="center">
+          <ComputerIcon />
+          <Typography variant="h5">
+            Hello World. <Link href="/about">About</Link>
+          </Typography>
+        </Stack>
+        {data && !error
+          ? <Typography variant="pre">{JSON.stringify({ data })}</Typography>
+          : !data && !error
+            ? <CircularProgress />
+            : <Typography variant="pre" sx={{ color: 'red' }}>
+              {JSON.stringify({ error })}
+            </Typography>
+        }
       </Stack>
     </Box>
   )
