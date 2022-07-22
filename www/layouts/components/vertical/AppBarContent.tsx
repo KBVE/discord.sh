1162d3950b373153b7f1f1c0d268f1a286a26da0
h0lybyte <admin@kbve.com>
2022-07-22 00:06:05 -0400

    Update AppBarContent.tsx

--- www/layouts/components/vertical/AppBarContent.tsx
+++ www/layouts/components/vertical/AppBarContent.tsx
@@ -63,12 +63,12 @@ const AppBarContent = (props: Props) => {
             target='_blank'
             rel='noreferrer'
             sx={{ mr: 4, display: 'flex' }}
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free'
+            href='https://github.com/kbve/discord.sh'
           >
             <img
               height={24}
               alt='github stars'
-              src='https://img.shields.io/github/stars/themeselection/materio-mui-react-nextjs-admin-template-free?style=social'
+              src='https://img.shields.io/github/stars/kbve/discord.sh?style=social'
             />
           </Box>
         )}
