1284a083bb2f6c4b8d90b2c1fafc427f412cb80b
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-23 01:45:50 -0500

    Creating data templates and formulating api strategy
    Co-authored-by: h0lybyte <admin@kbve.com>

---
+++ www/pages/roulette.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Roulette() {
+  return (
+    <div>
+      <div>Roulette</div>
+      <div>
+        Find a random discord server to join! Back to <Link href="/">Home</Link>
+      </div>
+    </div>
+  )
+}
