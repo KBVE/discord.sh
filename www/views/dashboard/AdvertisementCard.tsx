ccf6730633b72d126e291bc231e624cb61dfd1ed
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-22 20:59:58 -0500

    Ad tweak

--- www/views/dashboard/AdvertisementCard.tsx
+++ www/views/dashboard/AdvertisementCard.tsx
@@ -1,24 +1,11 @@
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 
 const StatisticsCard = () => {
   return (
     <Card>
-      <CardHeader
-        title='Advertisement'
-        titleTypographyProps={{
-          sx: {
-            mb: 2.5,
-            lineHeight: '2rem !important',
-            letterSpacing: '0.15px !important'
-          }
-        }}
-      />
-      <CardContent sx={{ pt: theme => `${theme.spacing(3)} !important` }}>
-        <Grid container spacing={[5, 0]}>
+      <CardContent>
           <iframe
           data-aa='2048992'
           src='//acceptable.a-ads.com/2048992'
@@ -30,7 +17,6 @@ const StatisticsCard = () => {
             overflow: 'hidden',
             backgroundColor: 'transparent',
           }}></iframe>
-        </Grid>
       </CardContent>
     </Card>
   )
