1fccb79320d17255c756d3c6c157da09c72126e0
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-22 19:37:06 -0500

    Took images from template and used dashboard on main page

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,44 +1,101 @@
-import React from 'react'
-import Link from 'next/link'
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
 
-// import _ from 'lodash'
+// ** Icons Imports
+import Poll from 'mdi-material-ui/Poll'
+import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
+import HelpCircleOutline from 'mdi-material-ui/HelpCircleOutline'
+import BriefcaseVariantOutline from 'mdi-material-ui/BriefcaseVariantOutline'
 
-// @mui/material
-import Box from '@mui/material/Box'
-import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
-import CircularProgress from '@mui/material/CircularProgress'
+// ** Custom Components Imports
+import CardStatisticsVerticalComponent from '@core/components/card-statistics/card-stats-vertical'
 
-// @mui/icons-material
-import ComputerIcon from '@mui/icons-material/Computer'
+// ** Styled Component Import
+import ApexChartWrapper from '@core/styles/libs/react-apexcharts'
 
-const Home = () => {
+// ** Demo Components Imports
+import Table from 'views/dashboard/Table'
+import Trophy from 'views/dashboard/Trophy'
+import TotalEarning from 'views/dashboard/TotalEarning'
+import StatisticsCard from 'views/dashboard/StatisticsCard'
+import WeeklyOverview from 'views/dashboard/WeeklyOverview'
+import DepositWithdraw from 'views/dashboard/DepositWithdraw'
+import SalesByCountries from 'views/dashboard/SalesByCountries'
 
-  const [timedOut, setTimedOut] = React.useState(false)
-  React.useEffect(() => {
-    const timer1 = setTimeout(() => setTimedOut(true), 5 * 1000)
-
-    return () => {
-      clearTimeout(timer1)
-    }
-  }, [])
-
+const Dashboard = () => {
   return (
-    <Box p={4}>
-      <Stack direction="column" spacing={4}>
-        <Stack direction="row" spacing={2} alignItems="center">
-          <ComputerIcon />
-          <Typography variant="h5">
-            Hello World. <Link href="/about">About</Link> <Link href="/kbve">KBVE</Link>
-          </Typography>
-        </Stack>
-        {timedOut
-          ? <Typography variant="body2">{JSON.stringify({ timedOut })}</Typography>
-          : <CircularProgress />
-        }
-      </Stack>
-    </Box>
+    <ApexChartWrapper>
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={4}>
+          <Trophy />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <StatisticsCard />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <WeeklyOverview />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <TotalEarning />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Grid container spacing={6}>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='$25.6k'
+                icon={<Poll />}
+                color='success'
+                trendNumber='+42%'
+                title='Total Profit'
+                subtitle='Weekly Profit'
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='$78'
+                title='Refunds'
+                trend='negative'
+                color='secondary'
+                trendNumber='-15%'
+                subtitle='Past Month'
+                icon={<CurrencyUsd />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='862'
+                trend='negative'
+                trendNumber='-18%'
+                title='New Project'
+                subtitle='Yearly Project'
+                icon={<BriefcaseVariantOutline />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='15'
+                color='warning'
+                trend='negative'
+                trendNumber='-18%'
+                subtitle='Last Week'
+                title='Sales Queries'
+                icon={<HelpCircleOutline />}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <SalesByCountries />
+        </Grid>
+        <Grid item xs={12} md={12} lg={8}>
+          <DepositWithdraw />
+        </Grid>
+        <Grid item xs={12}>
+          <Table />
+        </Grid>
+      </Grid>
+    </ApexChartWrapper>
   )
 }
 
-export default Home
+export default Dashboard
