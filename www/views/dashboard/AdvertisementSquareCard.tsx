a51cc644bd6b4a1493003f585767099d5fcd85b9
J. Zane Cook <90814067+jzanecook@users.noreply.github.com>
2022-07-22 23:55:07 -0500

    Fix square banner

--- www/views/dashboard/AdvertisementSquareCard.tsx
+++ www/views/dashboard/AdvertisementSquareCard.tsx
@@ -5,15 +5,15 @@ import CardContent from '@mui/material/CardContent'
 const AdvertisementSquareCard = () => {
   return (
     <Card>
-      <CardContent>
+      <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
         <iframe
           data-aa="2050168"
           src="//ad.a-ads.com/2050168?size=250x250&background_color=8959f5&text_color=bdbed4&title_color=ffffff&title_hover_color=ffffff&link_color=ffffff&link_hover_color=ffffff"
           style={{
             border: 0,
             padding: 0,
-            width: '100%',
-            height: '100%',
+            width: '250px',
+            height: '250px',
             overflow: 'hidden',
             backgroundColor: 'transparent',
           }}
