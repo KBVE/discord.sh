799217fe14e0369ec4810502944340654d661ce7
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-11-09 15:26:26 -0500

    Update _document.tsx

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
-const isProd = process.env.NODE_ENV === 'production'
-const temp = `${<base href="https://discord.sh/" />}`
+// const isProd = process.env.NODE_ENV === 'production'
+// const temp = `${<base href="https://discord.sh/" />}`
 
 // Config
 import themeConfig from 'configs/themeConfig'
@@ -14,7 +14,7 @@ class DiscordSHDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {isProd ? temp : ''}
+          {/* {isProd ? temp : ''} */}
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
           {/** This is where a favicon and other header specific items would go */}
